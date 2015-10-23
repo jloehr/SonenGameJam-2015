600fcb29533fb9d5e61a113475554a9bccabb762
Julian Löhr <julian.loehr@outlook.com>
2015-10-23 23:55:14 +0200

    Add Game Class

---
+++ SonenGameJam-2015/SonenGameJam-2015/js/Game/game.js
@@ -0,0 +1,27 @@
+﻿var Game = {
+    Canvas: null,
+    Context: null,
+    FixedTimestep: 30,
+
+    Init: function()
+    {
+        this.SetupCanvas();
+        window.setInterval(function () { Game.Update(); }, this.FixedTimestep);
+    },
+
+    SetupCanvas : function()
+    {
+        this.Canvas = document.getElementById("GameScreen");
+        this.Canvas.width = window.innerWidth;
+        this.Canvas.height = window.innerHeight;
+        this.Context = this.Canvas.getContext("2d");
+    },
+
+    Update : function()
+    {
+        this.Context.fillStyle = "#FF0000";
+        this.Context.fillRect(0, 0, this.Canvas.width, this.Canvas.height);
+    }
+
+
+}
--- SonenGameJam-2015/SonenGameJam-2015/js/default.js
+++ SonenGameJam-2015/SonenGameJam-2015/js/default.js
@@ -5,9 +5,11 @@
 
 	var app = WinJS.Application;
 	var activation = Windows.ApplicationModel.Activation;
+	var canvas;
 
 	app.onactivated = function (args) {
-		if (args.detail.kind === activation.ActivationKind.launch) {
+	    if (args.detail.kind === activation.ActivationKind.launch) {
+	        Game.Init();
 			if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
 				// TODO: This application has been newly launched. Initialize your application here.
 			} else {
@@ -22,7 +24,7 @@
 		// TODO: This application is about to be suspended. Save any state that needs to persist across suspensions here.
 		// You might use the WinJS.Application.sessionState object, which is automatically saved and restored across suspension.
 		// If you need to complete an asynchronous operation before your application is suspended, call args.setPromise().
-	};
+	}
 
 	app.start();
 })();

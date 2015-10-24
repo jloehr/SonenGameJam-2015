6191f71a3ed3405a1f35aabc149a314cd820356c
Julian Löhr <julian.loehr@outlook.com>
2015-10-24 13:43:42 +0200

    Fix drawing and updating by dirty pixels

--- SonenGameJam-2015/SonenGameJam-2015/js/Game/game.js
+++ SonenGameJam-2015/SonenGameJam-2015/js/Game/game.js
@@ -4,6 +4,7 @@
     FixedTimestep: 30,
     GrowMap: null,
     Background: null,
+    FrameCounter: 0,
 
     Init: function()
     {
@@ -27,24 +28,25 @@
             this.Background.data[i + 2] = 19;
             this.Background.data[i + 3] = 255;
         }
-
+        this.Context.putImageData(this.Background, 0, 0);
     },
 
     Tick : function()
     {
+        this.FrameCounter++;
+        console.log("Frame: " + this.FrameCounter);
         this.Update();
         this.Draw();
     },
 
     Update : function()
     {
-
+        this.GrowMap.Update();
     },
 
     Draw : function()
     {
-        this.Context.putImageData(this.Background, 0, 0);
-        this.GrowMap.Draw();
+        this.GrowMap.Draw(this.Background);
     }
 }
 
@@ -54,6 +56,7 @@ function GrowMap(Canvas, Context)
     this.Context = Context;
 
     this.ImageData = null;
+    this.DirtyPixels = [];
 
     this.Constructor = function()
     {
@@ -63,22 +66,57 @@ function GrowMap(Canvas, Context)
             this.ImageData.data[i + 0] = 0;
             this.ImageData.data[i + 1] = 255;
             this.ImageData.data[i + 2] = 0;
-            this.ImageData.data[i + 3] = 255;
+            this.ImageData.data[i + 3] = 0;
+        }
+
+        var X = Canvas.width / 2;
+        var Y = Canvas.height / 2;
+        var i = this.ToIndex(X, Y);
+        this.ImageData.data[i + 3] = 1;
+        this.DirtyPixels.push({ X : X, Y : Y });
+    }
+
+    this.Draw = function(Background)
+    {
+        for (var i = 0; i < this.DirtyPixels.length; i++)
+        {
+            var Element = this.DirtyPixels[i];
+            var PixelData = this.Context.getImageData(Element.X, Element.Y, 1, 1);
+
+            var i = this.ToIndex(Element.X, Element.Y);
+            var Blend = (this.ImageData.data[i + 3] / 255);
+            PixelData.data[0] = Background.data[i + 0] * (1 - Blend) + this.ImageData.data[i + 0] * Blend;
+            PixelData.data[1] = Background.data[i + 1] * (1 - Blend) + this.ImageData.data[i + 1] * Blend;
+            PixelData.data[2] = Background.data[i + 2] * (1 - Blend) + this.ImageData.data[i + 2] * Blend;
+
+            this.Context.putImageData(PixelData, Element.X, Element.Y);
         }
     }
 
-    this.Draw = function()
-    {
-        var CanvasData = this.Context.getImageData(0, 0, this.Canvas.width, this.Canvas.height);
-        for (var i = 0; i < CanvasData.data.length; i += 4)
+    this.Update = function()
+    {   
+        for (var i = this.DirtyPixels.length - 1; i >= 0; i--)
         {
-            var Blend = (this.ImageData.data[i + 3] / 255);
-            CanvasData.data[i + 0] = CanvasData.data[i + 0] * (1 - Blend) + this.ImageData.data[i + 0] * Blend;
-            CanvasData.data[i + 1] = CanvasData.data[i + 1] * (1 - Blend) + this.ImageData.data[i + 1] * Blend;
-            CanvasData.data[i + 2] = CanvasData.data[i + 2] * (1 - Blend) + this.ImageData.data[i + 2] * Blend;
+            var Element = this.DirtyPixels[i];
+            this.Regrow(i, Element.X, Element.Y);
+        }
+    }
+
+    this.Regrow = function(DirtyIndex, X, Y)
+    {
+        var i = this.ToIndex(X, Y);
+        if(this.ImageData.data[i + 3] == 255)
+        {
+            this.DirtyPixels.splice(DirtyIndex, 1);
+            return;
         }
 
-        this.Context.putImageData(CanvasData, 0, 0);
+        this.ImageData.data[i + 3] += 1;
+    }
+
+    this.ToIndex = function(X, Y)
+    {
+        return (Y * 4) * this.Canvas.width + (X * 4);
     }
 
     this.Constructor();
